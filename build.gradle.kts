af2ac0ad62f46d1a27a1b8b3def1d5191adf6631
Victor Adewumi <43085886+VictorOluwaseun@users.noreply.github.com>
2022-12-10 12:02:17 +0100

    Set up Library dependencies structure #DC-7 (#2)
    * Completed #DC-7
    
    * Removed gradle.xml #DC-7
    
    * Removed all .idea files #DC-7
    
    * #DC-7 Removed gradlew.bat
    
    * #DC-7 Removed gradlew.bat
    
    * Added .idea to gitignore #DC-7
    
    * Added .idea to gitignore #DC-7
    
    * #DC-7 Resolved build issues
    
    * #DC-7 Changed Moshi to Gson
    
    * #DC-7 Added a new line
    
    * #DC-7 fix max line length
    
    Co-authored-by: sfxvadewumi <vadewumi@seamfix.com>
    Co-authored-by: Oladapo Oladokun <oladokun104@gmail.com>

---
+++ build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    id("org.jlleitschuh.gradle.ktlint") version "11.0.0"
+}
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath(Build.androidBuildTools)
+        classpath(Build.hiltAndroidGradlePlugin)
+        classpath(Build.kotlinGradlePlugin)
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.20")
+        classpath("com.android.tools.build:gradle:7.3.1")
+
+        // NOTE: Do not place your application dependencies here; they belong
+        // in the individual module build.gradle files
+    }
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
