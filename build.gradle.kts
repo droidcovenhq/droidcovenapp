2db4b8b594bb0751cadf7850631cd830a0f34dc7
timkwali <timkwalit@accessbankplc.com>
2022-12-10 13:50:52 +0100

    add idea to gitignore

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,11 @@ buildscript {
         classpath(Build.hiltAndroidGradlePlugin)
         classpath(Build.kotlinGradlePlugin)
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.20")
+<<<<<<< HEAD
         classpath("com.android.tools.build:gradle:7.3.1")
+=======
+        classpath("com.android.tools.build:gradle:7.2.0")
+>>>>>>> snapshot
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
